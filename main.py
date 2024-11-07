f93d3e4db1b8c53fc098070f10d4945e59d4b5d5
Chive7840 <gcarte9@wgu.edu>
2024-11-07 15:07:54 -0800

    Created project structure and main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+### PDF Reader and Converter for the MetroGnome Application ###
+
+# Author: Tyler Carter
+
+# Used in ImageRecognition
+import cv2
+import numpy as np
+
+# used in PDF Conversion
+import pdf2image
+
+def main():
+    print("placeholder")
+
+if __name__ == "__main__":
+    main()
