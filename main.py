f4f9bd254e4d3f1122b73e523589c7e0a22566d8
Chive7840 <gcarte9@wgu.edu>
2024-11-07 15:47:52 -0800

    created setup.cfg

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import numpy as np
 
 # used in PDF Conversion
 import pdf2image
+import musescore
+
+# used in scraping sheet music PDFs
+from typing import Union, Optional, Any, List, Dict
 
 def main():
     print("placeholder")
